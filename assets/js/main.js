055312264db3663cfae02003986361f893fcfa9c
m-reza41 <m.reza@41studio.com>
2015-12-30 16:10:08 +0700

    add menu UX

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,4 +33,49 @@ $(document).ready(function(){
 			$(this).parent('ul').siblings('span.selected').html(sitem);
 			$(this).parent('ul').siblings('input').val(sid);
 		});
+
 });
+
+
+/* menu*/
+(function() {
+
+	var bodyEl = document.body,
+		content = document.querySelector( 'body' ),
+		openbtn = document.getElementById( 'open-button' ),
+		closebtn = document.getElementById( 'close-button' ),
+		isOpen = false;
+
+	function init() {
+		initEvents();
+	}
+
+	function initEvents() {
+		openbtn.addEventListener( 'click', toggleMenu );
+		if( closebtn ) {
+			closebtn.addEventListener( 'click', toggleMenu );
+		}
+
+		// close the menu element if the target it´s not the menu element or one of its descendants..
+		content.addEventListener( 'click', function(ev) {
+			var target = ev.target;
+			if( isOpen && target !== openbtn ) {
+				toggleMenu();
+			}
+		} );
+	}
+
+	function toggleMenu() {
+		if( isOpen ) {
+			classie.remove( bodyEl, 'show-menu' );
+		}
+		else {
+			classie.add( bodyEl, 'show-menu' );
+		}
+		isOpen = !isOpen;
+	}
+
+	init();
+
+})();
+
