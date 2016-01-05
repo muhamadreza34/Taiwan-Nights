8945ec7fee36176d548e92e102d47e1bdd2af86c
m-reza41 <m.reza@41studio.com>
2016-01-05 14:29:51 +0700

    add search ux

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,6 +109,14 @@ $(document).ready(function(){
 	}
 
 	init();
+
+	/*list js */
+	var options = {
+  valueNames: [ 'name', 'born' ]
+	};
+
+	var userList = new List('users', options);
+
 })();
 
 
